7d952e20bbacc93ec2700af6423e13aa4ebee91d
fawwazmurtaza7262 <fawwazmurtaza635@gmail.com>
2026-01-30 23:04:07 -0500

    created destination.js and added handler code.

---
+++ js/destination.js
@@ -0,0 +1,14 @@
+//Destination JavaScript File that will have all the required JS code for the destination page
+console.log("This is the destination js file");
+
+const cityInput = document.getElementById("cityInput");
+const daysRange = document.getElementById("daysRange");
+const daysText = document.getElementById("daysText");
+
+const budgetRange = document.getElementById("budgetRange");
+const budgetText = document.getElementById("budgetText");
+const budgetValue = document.getElementById("budgetValue");
+
+const currencySelect = document.getElementById("currencySelect");
+const genereateBtn = document.getElementById("generateBtn");
+
